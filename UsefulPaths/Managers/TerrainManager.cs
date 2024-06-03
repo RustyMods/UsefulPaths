c6ed7da8f5e67ebd65e1b266194f02603341ced7
malcolm <pei.malcolm@gmail.com>
2024-06-02 20:40:04 -0400

    Initial commit

---
+++ UsefulPaths/Managers/TerrainManager.cs
@@ -0,0 +1,276 @@
+﻿using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UsefulPaths.Managers;
+
+public static class TerrainManager
+{
+    [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
+    private static class Register_AirJordan
+    {
+        private static void Postfix(ObjectDB __instance)
+        {
+            if (!__instance || !ZNetScene.instance) return;
+            var airJordan = ScriptableObject.CreateInstance<AirJordan>();
+            airJordan.name = "SE_AirJordan";
+            airJordan.m_icon = null;
+            
+            if (__instance.m_StatusEffects.Contains(airJordan)) return;
+            __instance.m_StatusEffects.Add(airJordan);
+        }
+    }
+
+    [HarmonyPatch(typeof(TextsDialog), nameof(TextsDialog.AddActiveEffects))]
+    private static class Compendium_AddUsefulPath_Info
+    {
+        private static void Postfix(TextsDialog __instance)
+        {
+            if (!Player.m_localPlayer.GetSEMan().HaveStatusEffect("SE_AirJordan".GetStableHashCode())) return;
+            string texts = __instance.m_texts[0].m_text;
+
+            var se = Player.m_localPlayer.GetSEMan().GetStatusEffect("SE_AirJordan".GetStableHashCode());
+            
+            texts += $"<color=orange>Useful Paths: {se.m_name}</color>\n";
+            texts += se.GetTooltipString();
+
+            __instance.m_texts[0].m_text = texts;
+        }
+    }
+
+    private static float m_timer;
+    public static void UpdateStatusEffect(float dt)
+    {
+        if (!Player.m_localPlayer) return;
+
+        m_timer += dt;
+        if (m_timer < UsefulPathsPlugin.m_update.Value) return;
+        m_timer = 0.0f;
+        
+        SEMan man = Player.m_localPlayer.GetSEMan();
+        if (man == null) return;
+        
+        if (UsefulPathsPlugin.m_enabled.Value is UsefulPathsPlugin.Toggle.Off)
+        {
+            if (man.HaveStatusEffect("SE_AirJordan".GetStableHashCode()))
+            {
+                man.RemoveStatusEffect("SE_AirJordan".GetStableHashCode());
+            }
+        }
+        else
+        {
+            if (man.HaveStatusEffect("SE_AirJordan".GetStableHashCode())) return;
+            man.AddStatusEffect("SE_AirJordan".GetStableHashCode());
+        }
+    }
+}
+
+public class AirJordan : StatusEffect
+{
+    private float m_timer;
+    
+    private GroundTypes m_terrain = GroundTypes.None;
+    
+    public override void ModifySpeed(float baseSpeed, ref float speed, Character character, Vector3 dir)
+    {
+        if (m_terrain is GroundTypes.None) return;
+        float modifier = GetSpeedModifier(m_terrain);
+        speed *= modifier;
+    }
+
+    public override void ModifyRunStaminaDrain(float baseDrain, ref float drain)
+    {
+        if (m_terrain is GroundTypes.None) return;
+        float modifier = GetRunStaminaDrain(m_terrain);
+        drain *= modifier;
+    }
+
+    public override void ModifyStaminaRegen(ref float staminaRegen)
+    {
+        if (m_terrain is GroundTypes.None) return;
+        float modifier = GetStaminaRegen(m_terrain);
+        staminaRegen *= modifier;
+    }
+
+    public override void ModifyMaxCarryWeight(float baseLimit, ref float limit)
+    {
+        if (m_terrain is GroundTypes.None) return;
+        float modifier = GetMaxCarryWeight(m_terrain);
+        limit += modifier;
+    }
+
+    public override void ModifyJump(Vector3 baseJump, ref Vector3 jump)
+    {
+        if (m_terrain is GroundTypes.None) return;
+        float modifier = GetJumpModifier(m_terrain);
+        jump = new Vector3(jump.x * modifier, jump.y * modifier, jump.z * modifier);
+    }
+
+    public override void UpdateStatusEffect(float dt)
+    {
+        base.UpdateStatusEffect(dt);
+        m_timer += dt;
+        if (m_timer < UsefulPathsPlugin.m_update.Value) return;
+        m_timer = 0.0f;
+        
+        m_terrain = GetTerrain();
+        m_name = m_terrain is GroundTypes.None ? "" : m_terrain.ToString();
+    }
+
+    public override string GetTooltipString()
+    {
+        if (m_terrain is GroundTypes.None) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        stringBuilder.Append(FormatTooltip("$item_movement_modifier", GetSpeedModifier(m_terrain)));
+        stringBuilder.Append(FormatTooltip("$se_runstamina", GetRunStaminaDrain(m_terrain)));
+        stringBuilder.Append(FormatTooltip("$se_staminaregen", GetStaminaRegen(m_terrain)));
+        stringBuilder.Append(FormatTooltip("$se_max_carryweight", GetMaxCarryWeight(m_terrain)));
+        stringBuilder.Append(FormatTooltip("$se_jumpheight", GetJumpModifier(m_terrain)));
+        
+        return Localization.instance.Localize(stringBuilder.ToString());
+    }
+
+    private string FormatTooltip(string key, float modifier)
+    {
+        if (key is "$se_max_carryweight")
+        {
+            string symbol = modifier > 0f ? "+" : "";
+            return modifier == 0f ? "" : $"{key}: <color=orange>{symbol}{(int)modifier}\n";
+        }
+        else
+        {
+            float value = modifier * 100 - 100;
+            string symbol = modifier > 1f ? "+" : "";
+            return value == 0 ? "" :  $"{key}: <color=orange>{symbol}{(int)value}%</color>\n";
+        }
+    }
+
+    private float GetSpeedModifier(GroundTypes terrain)
+    {
+        return terrain switch
+        {
+            GroundTypes.Paved => UsefulPathsPlugin.m_speed[GroundTypes.Paved].Value,
+            GroundTypes.Dirt => UsefulPathsPlugin.m_speed[GroundTypes.Dirt].Value,
+            GroundTypes.Cultivated => UsefulPathsPlugin.m_speed[GroundTypes.Cultivated].Value,
+            GroundTypes.Mud => UsefulPathsPlugin.m_speed[GroundTypes.Mud].Value,
+            GroundTypes.Stone => UsefulPathsPlugin.m_speed[GroundTypes.Stone].Value,
+            GroundTypes.Wood => UsefulPathsPlugin.m_speed[GroundTypes.Wood].Value,
+            GroundTypes.Snow => UsefulPathsPlugin.m_speed[GroundTypes.Snow].Value,
+            GroundTypes.Metal => UsefulPathsPlugin.m_speed[GroundTypes.Metal].Value,
+            _ => 1f
+        };
+    }
+    
+    private float GetJumpModifier(GroundTypes terrain)
+    {
+        return terrain switch
+        {
+            GroundTypes.Paved => UsefulPathsPlugin.m_jump[GroundTypes.Paved].Value,
+            GroundTypes.Dirt => UsefulPathsPlugin.m_jump[GroundTypes.Dirt].Value,
+            GroundTypes.Cultivated => UsefulPathsPlugin.m_jump[GroundTypes.Cultivated].Value,
+            GroundTypes.Mud => UsefulPathsPlugin.m_jump[GroundTypes.Mud].Value,
+            GroundTypes.Stone => UsefulPathsPlugin.m_jump[GroundTypes.Stone].Value,
+            GroundTypes.Wood => UsefulPathsPlugin.m_jump[GroundTypes.Wood].Value,
+            GroundTypes.Snow => UsefulPathsPlugin.m_jump[GroundTypes.Snow].Value,
+            GroundTypes.Metal => UsefulPathsPlugin.m_jump[GroundTypes.Metal].Value,
+            _ => 1f
+        };
+    }
+    
+    private float GetMaxCarryWeight(GroundTypes terrain)
+    {
+        return terrain switch
+        {
+            GroundTypes.Paved => UsefulPathsPlugin.m_carryWeight[GroundTypes.Paved].Value,
+            GroundTypes.Dirt => UsefulPathsPlugin.m_carryWeight[GroundTypes.Dirt].Value,
+            GroundTypes.Cultivated => UsefulPathsPlugin.m_carryWeight[GroundTypes.Cultivated].Value,
+            GroundTypes.Mud => UsefulPathsPlugin.m_carryWeight[GroundTypes.Mud].Value,
+            GroundTypes.Stone => UsefulPathsPlugin.m_carryWeight[GroundTypes.Stone].Value,
+            GroundTypes.Wood => UsefulPathsPlugin.m_carryWeight[GroundTypes.Wood].Value,
+            GroundTypes.Snow => UsefulPathsPlugin.m_carryWeight[GroundTypes.Snow].Value,
+            GroundTypes.Metal => UsefulPathsPlugin.m_carryWeight[GroundTypes.Metal].Value,
+            _ => 1f
+        };
+    }
+
+    private float GetRunStaminaDrain(GroundTypes terrain)
+    {
+        return terrain switch
+        {
+            GroundTypes.Paved => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Paved].Value,
+            GroundTypes.Dirt => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Dirt].Value,
+            GroundTypes.Cultivated => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Cultivated].Value,
+            GroundTypes.Mud => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Mud].Value,
+            GroundTypes.Stone => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Stone].Value,
+            GroundTypes.Wood => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Wood].Value,
+            GroundTypes.Snow => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Snow].Value,
+            GroundTypes.Metal => UsefulPathsPlugin.m_runStaminaDrain[GroundTypes.Metal].Value,
+            _ => 1f
+        };
+    }
+
+    private float GetStaminaRegen(GroundTypes terrain)
+    {
+        return terrain switch
+        {
+            GroundTypes.Paved => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Paved].Value,
+            GroundTypes.Dirt => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Dirt].Value,
+            GroundTypes.Cultivated => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Cultivated].Value,
+            GroundTypes.Mud => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Mud].Value,
+            GroundTypes.Stone => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Stone].Value,
+            GroundTypes.Wood => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Wood].Value,
+            GroundTypes.Snow => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Snow].Value,
+            GroundTypes.Metal => UsefulPathsPlugin.m_staminaRegen[GroundTypes.Metal].Value,
+            _ => 1f
+        };
+    }
+
+    private GroundTypes GetTerrain()
+    {
+        if (!m_character.TryGetComponent(out FootStep component)) return GroundTypes.None;
+
+        FootStep.GroundMaterial material = component.GetGroundMaterial(component.m_character, component.m_character.transform.position);
+
+        if (material is FootStep.GroundMaterial.Grass or FootStep.GroundMaterial.GenericGround)
+        {
+            var paint = GetPaintType(component.m_character);
+            return paint switch
+            {   
+                TerrainModifier.PaintType.Dirt => GroundTypes.Dirt,
+                TerrainModifier.PaintType.Cultivate => GroundTypes.Cultivated,
+                TerrainModifier.PaintType.Paved => GroundTypes.Paved,
+                _ => GroundTypes.None
+            };
+        }
+
+        return material switch
+        {
+            FootStep.GroundMaterial.Stone => GroundTypes.Stone,
+            FootStep.GroundMaterial.Wood => GroundTypes.Wood,
+            FootStep.GroundMaterial.Snow => GroundTypes.Snow,
+            FootStep.GroundMaterial.Mud => GroundTypes.Mud,
+            FootStep.GroundMaterial.Metal => GroundTypes.Metal,
+            _ => GroundTypes.None
+        };
+    }
+    
+    private TerrainModifier.PaintType GetPaintType(Character character)
+    {
+        var ground = character.GetLastGroundCollider();
+        if (ground == null) return TerrainModifier.PaintType.Reset;
+        if (!ground.TryGetComponent(out Heightmap component)) return TerrainModifier.PaintType.Reset;
+        
+        component.WorldToVertexMask(character.transform.position, out int x, out int y);
+        Color pixels = component.m_paintMask.GetPixel(x, y);
+        if (pixels.r > 0.5) return TerrainModifier.PaintType.Dirt;
+        if (pixels.g > 0.5) return TerrainModifier.PaintType.Cultivate;
+        if (pixels.b > 0.5) return TerrainModifier.PaintType.Paved;
+        return TerrainModifier.PaintType.Reset;
+    }
+}
+
+public enum GroundTypes
+{
+    None, Paved, Dirt, Cultivated, Mud, Snow, Metal, Stone, Wood
+}
---
+++ UsefulPaths/Plugin.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using ServerSync;
+using UnityEngine;
+using UsefulPaths.Managers;
+
+namespace UsefulPaths
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class UsefulPathsPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "UsefulPaths";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "RustyMods";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource UsefulPathsLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        private static UsefulPathsPlugin _Plugin = null!;
+        public enum Toggle { On = 1, Off = 0 }
+        
+        public static readonly Dictionary<GroundTypes, ConfigEntry<float>> m_speed = new();
+        public static readonly Dictionary<GroundTypes, ConfigEntry<float>> m_staminaRegen = new();
+        public static readonly Dictionary<GroundTypes, ConfigEntry<float>> m_runStaminaDrain = new();
+        public static readonly Dictionary<GroundTypes, ConfigEntry<float>> m_carryWeight = new();
+        public static readonly Dictionary<GroundTypes, ConfigEntry<float>> m_jump = new();
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+        public static ConfigEntry<float> m_update = null!;
+        public static ConfigEntry<Toggle> m_enabled = null!;
+
+        private void InitConfigs()
+        {
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            m_update = config("2 - Settings", "Update Timer", 1f,
+                new ConfigDescription("Set the rate to check terrain", new AcceptableValueRange<float>(1f, 10f)));
+            m_enabled = config("2 - Settings", "Enabled", Toggle.On, "If on, plugin is active and enabled");
+
+            foreach (GroundTypes type in Enum.GetValues(typeof(GroundTypes)))
+            {
+                if (type is GroundTypes.None) continue;
+                ConfigEntry<float> speed = _Plugin.config(type.ToString(), "Speed Modifier", 1f,
+                    new ConfigDescription($"Set the speed modifier for {type.ToString()}",
+                        new AcceptableValueRange<float>(0f, 10f)));
+                m_speed[type] = speed;
+                ConfigEntry<float> staminaRegen = _Plugin.config(type.ToString(), "Stamina Regeneration", 1f,
+                    new ConfigDescription($"Set the stamina regeneration for {type.ToString()}",
+                        new AcceptableValueRange<float>(0f, 10f)));
+                m_staminaRegen[type] = staminaRegen;
+                ConfigEntry<float> runStaminaDrain = _Plugin.config(type.ToString(), "Run Stamina Drain", 1f,
+                    new ConfigDescription($"Set the run stamina drain for {type.ToString()}",
+                        new AcceptableValueRange<float>(0f, 10f)));
+                m_runStaminaDrain[type] = runStaminaDrain;
+                ConfigEntry<float> carryWeight = _Plugin.config(type.ToString(), "Max Carry Weight", 0f,
+                    new ConfigDescription($"Set the max carry weight of {type.ToString()}",
+                        new AcceptableValueRange<float>(-100f, 100f)));
+                m_carryWeight[type] = carryWeight;
+                ConfigEntry<float> jump = _Plugin.config(type.ToString(), "Jump Modifier", 1f,
+                    new ConfigDescription($"Set the jump modifier for {type.ToString()}",
+                        new AcceptableValueRange<float>(0f, 10f)));
+                m_jump[type] = jump;
+            }
+        }
+
+        public void Awake()
+        {
+            _Plugin = this;
+            InitConfigs();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+        
+        private void Update()
+        {
+            float dt = Time.deltaTime;
+            TerrainManager.UpdateStatusEffect(dt);
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                UsefulPathsLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                UsefulPathsLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                UsefulPathsLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+        
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order;
+            [UsedImplicitly] public bool? Browsable;
+            [UsedImplicitly] public string? Category;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
+        }
+
+        #endregion
+    }
+
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
+        }
+    }
+}
